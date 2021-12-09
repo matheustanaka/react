350fd87ba87de1d242cb46226888becab76d393b
Matheus Tanaka <matheustanakanog@gmail.com>
2021-12-09 17:17:09 -0300

    feat: add new state and props

--- src/App.js
+++ src/App.js
@@ -1,14 +1,56 @@
-import logo from './logo.svg';
 import './App.css';
 import Weather from './components/Weather';
+import {useState} from "react";
 
 const App = () => {
-  const forecast = ["Rainy", "Snow", "Sunny"]
+  // const forecast = ["Rainy", "Snow", "Sunny"]
+
+  const [forecasts, setForecast] = useState([
+    {
+      id: 1,
+      climate: "Rainy",
+      characteristics: "Wet 💦"
+    },
+    {
+      id: 2,
+      climate: "Snow",
+      characteristics: "Freezing  🥶"
+    },
+    {
+      id: 3,
+      climate: "Sunny",
+      characteristics: "Wet 🥵"
+    }
+  ])
+
+  const handleChangeForecast = () => {
+    setForecast([
+      {
+        id: 1,
+        climate: "Thunderstorm",
+        characteristics: "Stormy 🌩️"
+      },
+      {
+        id: 2,
+        climate: "Sunny",
+        characteristics: "Hot ☀️"
+      },
+      {
+        id: 3,
+        climate: "Strong Winds",
+        characteristics: "Windy 💨"
+      }
+    ])
+  }
 
   return (
     <div className='Weather'>
-      {forecast.map((climate) => <h1>{climate}</h1>)}
+      {forecasts.map((forecast) => (
+        <Weather key={forecast.id} forecast = {forecast} />
+      ))}
+      <button onClick = {handleChangeForecast}>Change Forecast</button>
     </div>
+    
   );
 }
 
--- src/components/Weather.js
+++ src/components/Weather.js
@@ -1,8 +1,10 @@
-
-const Weather = () => {
-    return (
-        <h1>{}</h1>
-    );
-}
+const Weather = ({ forecast }) => {
+  return (
+    <div className='Weather'>
+      <h1>{forecast.climate}</h1>
+      <p> Today is {forecast.characteristics}</p>
+    </div>
+  );
+};
 
 export default Weather;
