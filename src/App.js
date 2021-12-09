b6df036228764956685a365d4af74ae6eaa5e344
Matheus Tanaka <matheustanakanog@gmail.com>
2021-12-09 15:58:12 -0300

    feat: add first example of components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+import Weather from './components/Weather';
 
 const App = () => {
+  const forecast = ["Rainy", "Snow", "Sunny"]
+
   return (
-    <div className="App">
-      
+    <div className='Weather'>
+      {forecast.map((climate) => <h1>{climate}</h1>)}
     </div>
   );
 }
---
+++ src/components/Weather.js
@@ -0,0 +1,8 @@
+
+const Weather = () => {
+    return (
+        <h1>{}</h1>
+    );
+}
+
+export default Weather;
